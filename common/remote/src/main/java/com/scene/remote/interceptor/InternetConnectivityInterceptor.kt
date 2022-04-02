00864b104a37b0f8f33f433b6f6737c031fdad7e
Cafer Kaya <cafer.kaya@trendyol.com>
2022-04-02 12:26:28 +0300

    Fix lint issues

--- common/remote/src/main/java/com/scene/remote/interceptor/InternetConnectivityInterceptor.kt
+++ common/remote/src/main/java/com/scene/remote/interceptor/InternetConnectivityInterceptor.kt
@@ -1,6 +1,7 @@
 package com.scene.remote.interceptor
 
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import com.scene.remote.errorhandler.NoConnectionException
 import okhttp3.Interceptor
 import okhttp3.Interceptor.Chain
@@ -15,11 +16,20 @@ class InternetConnectivityInterceptor @Inject constructor(
 
     @Synchronized
     override fun intercept(chain: Chain): Response {
-        val noConnection = connectivityManager.activeNetworkInfo?.isConnected != true
-        if (noConnection) {
+        val networkCapabilities =
+            connectivityManager.activeNetwork ?: throw NoConnectionException(chain.request().url.toString())
+        val activeNetwork = connectivityManager.getNetworkCapabilities(networkCapabilities)
+            ?: throw NoConnectionException(chain.request().url.toString())
+        if (isNetworkHasInternetCapability(activeNetwork).not()) {
             throw NoConnectionException(chain.request().url.toString())
         }
 
         return chain.proceed(chain.request())
+    }
+
+    private fun isNetworkHasInternetCapability(network: NetworkCapabilities): Boolean {
+        return network.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                network.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                network.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
     }
 }
